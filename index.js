9ea408549238557df0ac6c94ebcff833051bcdb6
Suman Karmakar <karmakarsuman12138@gmail.com>
2026-03-04 18:18:07 +0530

    Update index.js
    add

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const elements = {
     progressSection: document.querySelector(".progress"),
     metricsGrid: document.querySelector(".metrics-grid"),
     externalSummary: document.querySelector(".external-summary"),
-    progrss: {
+    progress: {
         easy: document.querySelector(".easy-progress"),
         medium: document.querySelector(".medium-progress"),
         hard: document.querySelector(".hard-progress")
