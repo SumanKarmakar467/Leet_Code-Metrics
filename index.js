96a57fba22463a07ed63166ae0cc674cfa69598c
Suman Karmakar <karmakarsuman12138@gmail.com>
2026-03-04 18:16:04 +0530

    Update index.js
    add

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const elements  {
+const elements = {
     form: document.getElementById("search-form"),
     input: document.getElementById("user-input"),
     searchBtn: document.getElementById("search-btn"),
